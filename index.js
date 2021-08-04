9b8e55255e4f5225b8152204b351b7542bebb16a
Sam Wasden <samwasden@outlook.com>
2021-08-04 15:50:05 -0600

    finished inital assignment

---
+++ index.js
@@ -0,0 +1,41 @@
+let message = document.querySelector("#message")
+
+function addMovie(event) {
+    event.preventDefault()
+    let inputField = document.querySelector("input")
+    
+    let movie = document.createElement("li")
+    let movieTitle = document.createElement("span")
+    movieTitle.innerHTML = inputField.value
+    movieTitle.addEventListener("click", crossOffMovie)
+
+    movie.appendChild(movieTitle)
+
+    let deleteBtn = document.createElement('button')
+    deleteBtn.textContent = "X"
+    deleteBtn.addEventListener("click", deleteMovie)
+
+    movie.appendChild(deleteBtn)
+
+    let list = document.querySelector("ul")
+    list.appendChild(movie)
+    inputField.value = ""
+}
+
+let form = document.querySelector("form")
+form.addEventListener("submit", addMovie)
+
+function deleteMovie(event) {
+    event.target.parentNode.remove() 
+    message.textContent = "Movie deleted!"
+}
+
+function crossOffMovie(event) {
+    event.target.classList.toggle("checked")
+    if (event.target.classList.contains("checked")) {
+        message.textContent = "Movie watched!"
+    } else {
+        message.textContent = "Movie added back!"
+    }
+}
+
